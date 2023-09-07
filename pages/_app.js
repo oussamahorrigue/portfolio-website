cbffc9dcd8fc92921c773e0bd7c80ed57e9afd1c
oussamahorrigue <horrigueouss@gmail.com>
2023-09-07 19:03:05 +0200

    add meta tag to verify website ownership

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,19 @@ import  Transition  from '../components/Transition';
 import { useRouter } from 'next/router';
 import { AnimatePresence, motion } from 'framer-motion';
 import Head from 'next/head'
-
+import circles from "../assets/circles.png"
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
   <Layout>
     <Head>
     <title>Oussama Horrigue</title>
+    <meta name="google-site-verification" content="hWvja5zXBHRWOXK9h3GRJXzYx0XYii0PnJ_J3pJvgS4" />
+    <meta name="description" content="Oussama horrigue's portfolio website" />
+    
     </Head>
     <AnimatePresence mode="wait">
       <motion.div key={router.route} className='h-full'>
-      
         <Component {...pageProps} />
       </motion.div>
     </AnimatePresence>
