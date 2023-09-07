99671770f8b93d8c8acca2312418e2f1b0118262
oussamahorrigue <horrigueouss@gmail.com>
2023-09-07 14:40:56 +0200

    fix image

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,7 +1,7 @@
 // testimonial data
 const testimonialData = [
   {
-    image: '../public/images/farouk.png',
+    image: 'images/farouk.png',
     name: 'Farouk',
     position: 'Sales Manager',
     message:
