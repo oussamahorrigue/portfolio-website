c8d5f0093f4a07dc442ca62d2633fba17856083d
oussamahorrigue <horrigueouss@gmail.com>
2023-09-07 17:00:00 +0200

    google-site-verification

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,15 @@ import '../styles/globals.css';
 import  Transition  from '../components/Transition';
 import { useRouter } from 'next/router';
 import { AnimatePresence, motion } from 'framer-motion';
-
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
   <Layout>
+    <Head>
+    <title>Oussama Horrigue</title>
+    </Head>
     <AnimatePresence mode="wait">
       <motion.div key={router.route} className='h-full'>
       
